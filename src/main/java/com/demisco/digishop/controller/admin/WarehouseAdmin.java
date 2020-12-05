841bd31f8fac95747ad8884cb398c860796354f2
Ramin Zare <zare88@fastmail.com>
2020-12-06 00:27:06 +0100

    Fix a wrong import & add .gitignore file

--- src/main/java/com/demisco/digishop/controller/admin/WarehouseAdmin.java
+++ src/main/java/com/demisco/digishop/controller/admin/WarehouseAdmin.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import sun.plugin.liveconnect.SecurityContextHelper;
 
 import java.security.Principal;
 
